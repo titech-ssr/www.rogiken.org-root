0970a92ab0a240eb290c57318eb62b8fc9c58917
kento <tajima.tajiken@gmail.com>
2024-04-05 19:50:02 +0900

    ⚡️ fullcalendarのCSSとHTMLを更新

---
+++ js/fullcalendar/calendar.js
@@ -0,0 +1,33 @@
+document.addEventListener('DOMContentLoaded', function() {
+  var calendarEl = document.getElementById('calendar');
+
+  var calendar = new FullCalendar.Calendar(calendarEl, {
+      locale: 'ja',
+      headerToolbar: {
+          left: 'prev,next today',
+          center: 'title',
+          right: 'dayGridMonth,timeGridWeek,timeGridDay'
+      },
+      dayCellContent: function(arg){
+        return arg.date.getDate();
+      },
+      buttonText: {  // ボタンのテキストをカスタマイズ
+        today: '今日',
+        month: '月',
+        week: '週',
+        day: '日'
+    },
+    initialView: 'dayGridMonth',
+    googleCalendarApiKey: "AIzaSyCvmCQKqGdRBB2ytmYEgBd4_lnMa24LwM4",
+    eventSources: [
+    {
+      googleCalendarId: "tajima.tajiken@gmail.com",
+      className: "my-events"
+    },
+    {
+			googleCalendarId: 'ja.japanese#holiday@group.v.calendar.google.com'
+		}
+  ]
+  });
+  calendar.render();
+});
